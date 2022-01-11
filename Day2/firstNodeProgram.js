f8106e4583079a319addea0bb6e70c9ef286f93a
SaudSaleem <saud.saleem@invozone.com>
2022-01-11 11:15:21 +0500

    add git ignore

--- Day2/firstNodeProgram.js
+++ Day2/firstNodeProgram.js
@@ -1,7 +1,20 @@
+let Sequelize = require('sequelize')
 var http = require('http');
 
-http.createServer(function (req, res) {
+http.createServer(async function (req, res) {
   console.log('hello my first node program')  
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello SAUD');
+  // Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('saud_db', 'saudsaleem', '12345678', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+ // const sequelize = new Sequelize('postgres://user:saudsaleem:12345678/my-first-db') // Example for postgres
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }).listen(8080);
--- Day3/firstSequlize.js
+++ Day3/firstSequlize.js
@@ -5,16 +5,21 @@ http.createServer(async function (req, res) {
   console.log('hello my first node program')  
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello SAUD');
-  // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('saud_db', 'saudsaleem', '12345678', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
- // const sequelize = new Sequelize('postgres://user:saudsaleem:12345678/my-first-db') // Example for postgres
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+  if(req === '/')
+  {
+    res.write('hello orl'),
+    res.end();
   }
+//   // Option 3: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('saud_db', 'saudsaleem', '12345678', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+//  // const sequelize = new Sequelize('postgres://user:saudsaleem:12345678/my-first-db') // Example for postgres
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
 }).listen(8080);
